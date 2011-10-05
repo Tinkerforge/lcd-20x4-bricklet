7cea9b0d934b50c6a4d99758c0a59f6f647f8eea
Olaf Lüke <olaf@tinkerforge.com>
2011-10-06 01:00:34 +0200

    add tinkerforge package to python imports

--- software/examples/python/example_button_callbacks.py
+++ software/examples/python/example_button_callbacks.py
@@ -5,8 +5,8 @@ HOST = "localhost"
 PORT = 4223
 UID = "XYZ" # Change to your UID
 
-from ip_connection import IPConnection
-from bricklet_lcd_20x4 import LCD20x4
+from tinkerforge.ip_connection import IPConnection
+from tinkerforge.bricklet_lcd_20x4 import LCD20x4
 
 # Callback functions for button status
 def cb_pressed(i):
--- software/examples/python/example_hello_world.py
+++ software/examples/python/example_hello_world.py
@@ -5,8 +5,8 @@ HOST = "localhost"
 PORT = 4223
 UID = "XYZ" # Change to your UID
 
-from ip_connection import IPConnection
-from bricklet_lcd_20x4 import LCD20x4
+from tinkerforge.ip_connection import IPConnection
+from tinkerforge.bricklet_lcd_20x4 import LCD20x4
 
 if __name__ == "__main__":
     ipcon = IPConnection(HOST, PORT) # Create ip connection to brickd
