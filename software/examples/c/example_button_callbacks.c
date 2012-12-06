cf7ae67e962d509f7da6960610e398a07885f4a0
Matthias Bolte <matthias@tinkerforge.com>
2012-12-06 15:34:02 +0100

    Fix C example

--- software/examples/c/example_button_callbacks.c
+++ software/examples/c/example_button_callbacks.c
@@ -10,10 +10,14 @@
 
 // Callback functions for button status
 void cb_pressed(uint8_t i, void *user_data) {
+	(void)user_data; // avoid unused parameter warning
+
 	printf("Pressed: %d\n", i);
 }
 
 void cb_released(uint8_t i, void *user_data) {
+	(void)user_data; // avoid unused parameter warning
+
 	printf("Released: %d\n", i);
 }
 
@@ -24,7 +28,7 @@ int main() {
 
 	// Create device object
 	LCD20x4 lcd;
-	lcd_20x4_create(&lcd, UID, &ipcon); 
+	lcd_20x4_create(&lcd, UID, &ipcon);
 
 	// Connect to brickd
 	if(ipcon_connect(&ipcon, HOST, PORT) < 0) {
@@ -34,15 +38,15 @@ int main() {
 	// Don't use device before ipcon is connected
 
 	// Register button status callbacks to cb_pressed and cb_released
-	lcd_20x4_register_callback(&lcd, 
-	                           LCD_20X4_CALLBACK_BUTTON_PRESSED, 
+	lcd_20x4_register_callback(&lcd,
+	                           LCD_20X4_CALLBACK_BUTTON_PRESSED,
 	                           cb_pressed,
-							   NULL);
+	                           NULL);
 
-	lcd_20x4_register_callback(&lcd, 
-	                           LCD_20X4_CALLBACK_BUTTON_RELEASED, 
+	lcd_20x4_register_callback(&lcd,
+	                           LCD_20X4_CALLBACK_BUTTON_RELEASED,
 	                           cb_released,
-							   NULL);
+	                           NULL);
 
 	printf("Press key to exit\n");
 	getchar();
