6f7aec429f7d79dd10762e8026e500f5d51adc61
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2014-02-11 16:55:21 +0100

    javascript: short examples

--- software/examples/javascript/ExampleButtonCallbacks.js
+++ software/examples/javascript/ExampleButtonCallbacks.js
@@ -10,9 +10,7 @@ var lcd = new BrickletLCD20x4(UID, ipcon);// Create device object
 
 ipcon.connect(HOST, PORT,
     function(error) {
-        if(error === IPConnection.ERROR_ALREADY_CONNECTED) {
-            console.log('Error: Already connected');        
-        }
+        console.log('Error: '+error);        
     }
 );// Connect to brickd
 
@@ -31,14 +29,10 @@ lcd.on(BrickletLCD20x4.CALLBACK_BUTTON_RELEASED,
 );
 
 console.log("Press any key to exit ...");
-process.stdin.on('data', function(data) {
-	    ipcon.disconnect(
-            function(error) {
-                if(error === IPConnection.ERROR_NOT_CONNECTED) {
-                    console.log('Error: Not connected');        
-                }
-            }
-        );
-process.exit(0);
-});
+process.stdin.on('data',
+    function(data) {
+        ipcon.disconnect();
+        process.exit(0);
+    }
+);
 
