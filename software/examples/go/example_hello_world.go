4957b380a2ae0fe1125f3f632e9f1c5e5ba2cbb9
Erik Fleckstein <erik@tinkerforge.com>
2018-12-14 16:11:56 +0100

    Add go examples.

---
+++ software/examples/go/example_hello_world.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"tinkerforge/ipconnection"
+	"tinkerforge/lcd_20x4_bricklet"
+)
+
+const ADDR string = "localhost:4223"
+const UID string = "XYZ" // Change XYZ to the UID of your LCD 20x4 Bricklet.
+
+func main() {
+	ipcon := ipconnection.New()
+	defer ipcon.Close()
+	lcd, _ := lcd_20x4_bricklet.New(UID, &ipcon) // Create device object.
+
+	ipcon.Connect(ADDR) // Connect to brickd.
+	defer ipcon.Disconnect()
+	// Don't use device before ipcon is connected.
+
+	// Turn backlight on
+	lcd.BacklightOn()
+
+	// Write "Hello World"
+	lcd.WriteLine(0, 0, "Hello World")
+
+	fmt.Print("Press enter to exit.")
+	fmt.Scanln()
+
+}
