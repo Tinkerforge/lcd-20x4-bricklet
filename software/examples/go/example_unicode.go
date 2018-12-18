4bdf5b3107112d234e7894d8c89bfa0768d2c79c
Erik Fleckstein <erik@tinkerforge.com>
2018-12-18 14:56:41 +0100

    go: Change import path.

--- software/examples/go/example_unicode.go
+++ software/examples/go/example_unicode.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-"tinkerforge/ipconnection"
-    "tinkerforge/lcd_20x4_bricklet"
+    "github.com/tinkerforge/go-api-bindings/ipconnection"
+    "github.com/tinkerforge/go-api-bindings/lcd_20x4_bricklet"
 )
 
 const ADDR string = "localhost:4223"
